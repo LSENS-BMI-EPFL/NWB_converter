3a659b9f6fa8d9a52a70fec2092c0b91c5cc51fe
Anthony Renard <aprrenard@protonmail.com>
2023-06-12 10:20:14 +0200

    Convertion metadata.

--- NWB_conversion.py
+++ NWB_conversion.py
@@ -1,16 +1,18 @@
+"""_summary_
+"""
+import os
 from converters.subject_to_nwb import create_nwb_file
 from converters.ci_movie_to_nwb import convert_ci_movie
 from converters.suite2p_to_nwb import convert_suite2p_data
 from converters.nwb_saving import save_nwb_file
 from converters.behavior_to_nwb import convert_behavior_data
-from continuous_log_processing.continuous_log_analysis import analyze_continuous_log
-import os
+from continuous_log_analysis import analyze_continuous_log
+from utils.behavior_converter_misc import find_training_days
+from utils.server_paths import get_subject_data_folder, get_subject_analysis_folder, get_nwb_folder
 
 
-def convert_data_to_nwb(subject_data_yaml, session_data_yaml, log_yaml_file, two_p_yaml_file, suite2p_folder,
-                        behavior_results_file, bhv_mapping_file, output_folder):
+def convert_data_to_nwb(config_file, output_folder):
     """
-
     :param subject_data_yaml: Path to the subject data yaml config file containing mouse ID and metadata
     :param session_data_yaml: Path to the session data yaml config file containing session metadata
     :param log_yaml_file: Path to the yaml config file used to analyse continuous logging and behavior
@@ -22,57 +24,58 @@ def convert_data_to_nwb(subject_data_yaml, session_data_yaml, log_yaml_file, two
     :return: NWB file
     """
 
-    print(f"Start NWB conversion")
-    print(f" ")
-    print(f"Extract timestamps from continuous log")
-    timestamps_dict, n_frames_dict = analyze_continuous_log(config_file=log_yaml_file, do_plot=False,
-                                                            plot_start=None, plot_stop=None)
+    print("Start NWB conversion")
+    print(" ")
+    print("Extract timestamps from continuous log")
+    timestamps_dict, n_frames_dict = analyze_continuous_log(config_file=config_file,
+                                                            do_plot=False, plot_start=None,
+                                                            plot_stop=None, camera_filtering=False)
 
-    print(f" ")
-    print(f"Open NWB file and add metadata")
-    nwb_file = create_nwb_file(subject_data_yaml_file=subject_data_yaml,
-                               session_data_yaml_file=session_data_yaml)
+    print(" ")
+    print("Open NWB file and add metadata")
+    nwb_file = create_nwb_file(config_file=config_file)
 
-    print(f" ")
-    print(f"Convert CI movie")
-    convert_ci_movie(nwb_file=nwb_file,
-                     two_p_yaml_file=two_p_yaml_file,
-                     log_yaml_file=log_file,
-                     movie_format='link')
+    # # TODO: update/remove the link to motion corrected ci tiff.
+    # print(" ")
+    # print("Convert CI movie")
+    # convert_ci_movie(nwb_file=nwb_file, config_file=config_file,
+    #                  movie_format='link')
 
-    print(f" ")
-    print(f"Convert Suite2p data")
-    if suite2p_folder is not None:
-        convert_suite2p_data(nwb_file=nwb_file,
-                             suite2p_folder=suite2p_folder,
-                             ci_frame_timestamps=timestamps_dict['galvo_position'][0:-2])
-    else:
-        print(f"No Suite2p data to add")
+    # # TODO: find suite2P folder with config file.
+    # print(" ")
+    # print("Convert Suite2p data")
+    # if suite2p_folder is not None:
+    #     convert_suite2p_data(nwb_file=nwb_file,
+    #                          suite2p_folder=suite2p_folder,
+    #                          ci_frame_timestamps=timestamps_dict['galvo_position'][0:-2])
+    # else:
+    #     print("No Suite2p data to add")
 
-    print(f" ")
-    print(f"Convert Behavior data")
-    convert_behavior_data(nwb_file, timestamps_dict, behavior_results_file, bhv_mapping_file)
+    print(" ")
+    print("Convert Behavior data")
+    convert_behavior_data(nwb_file, timestamps_dict, config_file)
 
-    print(f" ")
-    print(f"Saving NWB file")
+    print(" ")
+    print("Saving NWB file")
     save_nwb_file(nwb_file=nwb_file, output_folder=output_folder)
 
 
-# Run the conversion
-root_path = "C:/Users/rdard/Documents/NWB_to_create"
-mouse_id = "PB124"
-day = "whisker_day0"
-bhv_session = "PB124_20230404_141456"
-subject_yaml = os.path.join(root_path, mouse_id, day, "subject_data.yml")
-session_yaml = os.path.join(root_path, mouse_id, day, "session_data.yml")
-log_file = os.path.join(root_path, mouse_id, day, f"log_file_config_{bhv_session}.yml")
-two_p_yml = os.path.join(root_path, mouse_id, day, "2P_setup.yml")
-s2p_folder = os.path.join(root_path, mouse_id, day, "suite2p")
-# s2p_folder = None
-bhv_file = os.path.join(root_path, mouse_id, day, bhv_session, "results.csv")
-bhv_trials_names = os.path.join(root_path, mouse_id, day, "trial_type_mapping.yml")
-nwb_output_folder = "C:/Users/rdard/Documents/NWB_files"
-
-convert_data_to_nwb(subject_data_yaml=subject_yaml, session_data_yaml=session_yaml, log_yaml_file=log_file,
-                    two_p_yaml_file=two_p_yml, suite2p_folder=s2p_folder, behavior_results_file=bhv_file,
-                    bhv_mapping_file=bhv_trials_names, output_folder=nwb_output_folder)
+if __name__ == '__main__':
+    # Run the conversion
+    mouse_id = 'AR091'
+    
+    data_folder = get_subject_data_folder(mouse_id)
+    analysis_folder = get_subject_analysis_folder(mouse_id)
+    nwb_folder = get_nwb_folder(mouse_id)
+    
+    # Find session list and session description.
+    training_days = find_training_days(mouse_id, data_folder)
+    
+    # Create NWB by looping over sessions.
+    for isession, iday in training_days:
+        # Find yaml config file and behavior results for this session.
+        config_yaml = os.path.join(analysis_folder, isession, f"config_{isession}.yaml")
+        bin_file = os.path.join(data_folder, 'Training', isession, 'results.csv')
+        # Make conversion.
+        print(f"Session: {isession}")
+        convert_data_to_nwb(config_file=config_yaml, output_folder=nwb_folder)
---
+++ make_yaml_config.py
@@ -0,0 +1,187 @@
+"""_summary_
+"""
+import os
+import datetime
+import json
+import yaml
+import pandas as pd
+from utils.behavior_converter_misc import find_training_days
+from utils.server_paths import get_subject_data_folder, get_subject_analysis_folder
+
+def make_yaml_config(subject_id, session_id, session_description, input_folder, output_folder,
+                     mouse_line='C57BL/6', gmo=True):
+    """_summary_
+
+    Args:
+        subject_id (_type_): _description_
+        session_id (_type_): _description_
+        session_description (_type_): _description_
+        mouse_line (str, optional): _description_. Defaults to 'C57BL/6'.
+        gmo (bool, optional): _description_. Defaults to True.
+        server_mount (_type_, optional): _description_. Defaults to None.
+    """
+
+    # Subject metadata.
+    # #################
+
+    # Select most recent metadata export from SLIMS folder.
+    slims_csv = sorted(os.listdir(os.path.join(input_folder, 'SLIMS')))[-1]
+    slims_csv_path = os.path.join(input_folder, 'SLIMS', slims_csv)
+    slims = pd.read_csv(slims_csv_path, sep=';', engine='python')
+    slims = slims.loc[slims.cntn_cf_mouseName==mouse_id]
+
+    subject_metadata = {
+        'mouse_id': subject_id,
+        'age__reference': 'birth',
+        'line': mouse_line,
+        'sex': slims['cntn_cf_sex'].values[0].capitalize(),
+        'species': 'Mus musculus',
+        'subject_id': slims['cntn_cf_mouseName'].values[0],
+        }
+
+    # Compute age in days at session time.
+    session_date = session_id.split('_')[1]
+    session_date = datetime.datetime.strptime(session_date, "%Y%m%d")
+    birth_date = slims['cntn_cf_dateofbirth'].values[0]
+    birth_date = datetime.datetime.strptime(birth_date, "%d/%m/%Y")
+    days = (session_date - birth_date).days
+    subject_metadata['age'] = f"P{days}D"
+    subject_metadata['date_of_birth'] = datetime.datetime.strftime(birth_date, '%m/%d/%Y')
+
+    # Add strain from Slims if not WT mouse.
+    # Wether mouse is WT in not in the default Slims metadata, so use 'gmo' parameter.
+    # If you want to check if the mouse has the mutation with Slims, export the mutation column.
+    if gmo:
+        subject_metadata['genotype'] = slims['cntn_cf_strain'].values[0]
+    else:
+        subject_metadata['genotype'] = 'WT'
+
+    # Add weight at the beginning of the session from json config file.
+    json_path = os.path.join(input_folder, 'Training', session_id, 'session_config.json')
+    with open(json_path, 'r') as f:
+        json_config = json.load(f)
+    if 'mouse_weight_before' in json_config:
+        subject_metadata['weight'] = json_config['mouse_weight_before']
+    else:
+        subject_metadata['weight'] = 'na'
+
+
+    # Session metadata.
+    # #################
+
+    # session data
+    session_metadata = {
+        'identifier': session_id,  # key to name the NWB file
+        'session_id': session_id,
+        'session_start_time': session_id.split('_')[1] + ' ' + session_id.split('_')[2],
+        'session_description': session_description,
+        'experimenter': subject_id[:2],
+        'institution': 'EPFL',
+        'lab': 'LSENS',
+        'experiment_description': 'na',
+        'keywords': [],
+        'notes': 'na',
+        'pharmacology': 'na',
+        'protocol': 'na',
+        'related_publications': 'na',
+        'source_script': 'na',
+        'source_script_file_name': 'na',
+        'surgery': 'na',
+        'virus': 'na',
+        'stimulus_notes': 'na',
+        'slices': 'na',
+        }
+
+
+    # Log continuous metadata.
+    # ########################
+
+    scanimage_dict = {
+        'theoretical_ci_sampling_rate': 30,
+        'zoom': 3
+        }
+
+    channels_dict = {
+        'trial_TTL': 2,
+        'lick_trace': 0,
+        'galvo_position': 1,
+        'cam1': 3,
+        'cam2': 4,
+        }
+
+    threshold_dict = {
+        'trial_TTL': 4,
+        'cam1': 2,
+        'cam2': 2,
+        'galvo_position': {
+            '1': 2,
+            '2': 1.2,
+            '3': 0.9,
+            },
+        }
+
+    log_continuous_metadata = {
+        'scanimage_dict': scanimage_dict,
+        'channels_dict': channels_dict,
+        'threshold_dict': threshold_dict,
+        }
+
+
+    # Trial outcome mapping.
+    # ######################
+
+    trial_map = {
+        0: 'whisker_miss',
+        1: 'auditory_miss',
+        2: 'whisker_hit',
+        3: 'auditory_hit',
+        4: 'correct_rejection',
+        5: 'false_alarm',
+        6: 'early_lick',
+        }
+
+
+    # 2P imaging metadata.
+    # ####################
+
+    two_photon_metadata = {
+        'device': '2P microscope setup 1',
+        'emission_lambda': 510.0,
+        'excitation_lambda': 940.0,
+        'image_plane_location': 'S1_L2/3',
+        'indicator': 'GCaMP8m',
+    }
+
+
+    # Write to yaml file.
+    # ###################
+
+    main_dict = {
+        'subject_metadata': subject_metadata,
+        'session_metadata': session_metadata,
+        'log_continuous_metadata': log_continuous_metadata,
+        'trial_map': trial_map,
+        '2P_metadata': two_photon_metadata,
+    }
+
+    analysis_session_folder = os.path.join(output_folder, session_id)
+    if not os.path.exists(analysis_session_folder):
+        os.makedirs(analysis_session_folder)
+    with open(os.path.join(analysis_session_folder, f"config_{session_id}.yaml"), 'w') as stream:
+        yaml.dump(main_dict, stream, default_flow_style=False, explicit_start=True)
+
+
+if __name__ == '__main__':
+    mouse_id = 'AR091'
+
+    # Find data and analysis folders on server for that mouse.
+    data_folder = get_subject_data_folder(mouse_id)
+    analysis_folder = get_subject_analysis_folder(mouse_id)
+    if not os.path.exists(analysis_folder):
+        os.makedirs(analysis_folder)
+    
+    # Make config files.
+    training_days = find_training_days(mouse_id, data_folder)
+    for session_id, day in training_days:
+        make_yaml_config(mouse_id, session_id, day, data_folder, analysis_folder,
+                        mouse_line='C57BL/6', gmo=True)
