4dd617380c615c4427505cea59be808676454fe1
Robin Dard <robin.dard@epfl.ch>
2023-09-30 10:36:43 +0200

    update threshold for early licks detection

--- utils/continuous_processing.py
+++ utils/continuous_processing.py
@@ -280,7 +280,7 @@ def extract_timestamps(continuous_data_dict, threshold_dict, ni_session_sr, scan
                 # Detection of early licks
                 iti = np.array([on_off_timestamps[i+1][0] - on_off_timestamps[i][1]
                                 for i in range(len(on_off_timestamps) - 1)])
-                early_licks = np.where(iti < 0.25)[0]  # reset trial signal in less than 0.25 s (specific to early lick)
+                early_licks = np.where(iti < 0.4)[0]  # reset trial signal in less than 0.25 s (specific to early lick)
                 print(f"{len(early_licks)} early licks")
 
                 if len(early_licks) > 0:
@@ -293,7 +293,7 @@ def extract_timestamps(continuous_data_dict, threshold_dict, ni_session_sr, scan
 
             if key in ["trial_TTL"] and binary_data[-1] == 1:
                 print(f"Session likely stopped before end of last {key}")
-                filtered_on_off_timestamps = on_off_timestamps[0: -1] # remove last timestamp that signals session end
+                filtered_on_off_timestamps = on_off_timestamps[0: -1]  # remove last timestamp that signals session end
                 on_off_timestamps = filtered_on_off_timestamps
 
             timestamps_dict[key] = on_off_timestamps
