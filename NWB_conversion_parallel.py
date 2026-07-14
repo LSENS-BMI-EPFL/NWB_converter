73166f1b898f01120be3dd4dc4b761fb5a47a7a6
abisi <axel.bisi@epfl.ch>
2026-07-14 13:40:16 +0200

    minor

--- NWB_conversion_parallel.py
+++ NWB_conversion_parallel.py
@@ -14,7 +14,7 @@ import utils.utils_gf as utils_gf
 from continuous_log_analysis import analyze_continuous_log
 from converters.behavior_to_nwb import convert_behavior_data
 from converters.ci_movie_to_nwb import convert_ci_movie
-from converters.ephys_to_nwb import convert_ephys_recording
+from converters.ephys_to_nwb_test import convert_ephys_recording #todo: finish
 from converters.nwb_saving import save_nwb_file
 from converters.subject_to_nwb import create_nwb_file
 from converters.suite2p_to_nwb import convert_suite2p_data
@@ -36,7 +36,8 @@ def convert_data_to_nwb(config_file, output_folder, with_time_string=True, exper
 
     # Read config file to know what data to convert.
     with open(config_file, 'r', encoding='utf8') as stream:
-        config_dict = yaml.safe_load(stream)
+        config_dict = yaml.load(stream, Loader=yaml.UnsafeLoader)
+        #config_dict = yaml.safe_load(stream)
 
     print(" ")
     print("Start NWB conversion")
@@ -59,8 +60,7 @@ def convert_data_to_nwb(config_file, output_folder, with_time_string=True, exper
 
     print(" ")
     print("Convert behavior data")
-    convert_behavior_data(
-        nwb_file=nwb_file, timestamps_dict=timestamps_dict, config_file=config_file)
+    convert_behavior_data(nwb_file=nwb_file, timestamps_dict=timestamps_dict, config_file=config_file)
 
     if config_dict.get("two_photon_metadata") is not None:
         print(" ")
@@ -78,10 +78,13 @@ def convert_data_to_nwb(config_file, output_folder, with_time_string=True, exper
         if config_dict.get("ephys_metadata").get("processed") == 1:
              print(" ")
              print("Convert extracellular electrophysiology data")
+             add_recordings=False
+             if add_recordings:
+                 output_folder = Path(output_folder) / 'nwb_recordings'
              convert_ephys_recording(nwb_file=nwb_file,
                                      config_file=config_file,
-                                     experimenter=experimenter,
-                                     )
+                                     add_recordings=add_recordings,
+                                     experimenter=experimenter)
 
     # Check we are on WF computer
     platform_info = platform.uname()
@@ -131,44 +134,123 @@ def convert_single_session(config_yaml, nwb_folder, experimenter_full, isession)
     except Exception as e:
         print(f"Error processing {isession}: {str(e)}")
         return f"Error processing {isession}: {str(e)}"
-
 if __name__ == '__main__':
 
     # Run the conversion
-    mouse_ids = [
-        'PB191', 
-        'PB192',
-        'PB201',
-        'PB198',
-        'PB195',
-        'PB200',
-        'PB197',
-        'PB196',
-        'PB193',
-        'PB194',
-        ]
-
-    sessions_to_do = [
-        'PB191_20241210_110601',
-        'PB192_20241211_113347',
-        'PB193_20241218_135125',
-        'PB194_20241218_161235',
-        'PB195_20241214_114410',
-        'PB196_20241217_144715',
-        'PB197_20241216_155436',
-        'PB198_20241213_142448',
-        'PB200_20241216_112934',
-        'PB201_20241212_192123',
+    mouse_ids_mh = [
+        'MH004',
+        'MH006',
+        'MH007',
+        'MH008',
+        'MH009',
+        'MH010',
+        'MH011',
+        'MH013',
+        'MH014',
+        'MH015',
+        'MH016',
+        'MH017',
+        'MH018',
+        'MH019',
+        'MH020',
+        'MH021',
+        'MH022',
+        'MH023',
+        'MH025',
+        'MH026',
+        'MH027',
+        'MH028',
+        'MH029',
+        'MH030',
+        #'MH031',
+        'MH032',
+        'MH034',
+        'MH035',
+        'MH036',
+        'MH037',
+        #'MH038',
+        'MH039',
+        'MH062',
+        'MH064',
+        'MH065',
+        'MH068',
+        'MH069',
+        'MH070',
     ]
+    mouse_ids = [
+        'AB077',
+        'AB079',
+        'AB080',
+        'AB082',
+        'AB085',
+        'AB086',
+        'AB087',
+        'AB091',
+        'AB092',
+        'AB093',
+        'AB094',
+        'AB095',
+        'AB102',
+        'AB104',
+        'AB107',
+        'AB116',
+        'AB117',
+        'AB119',
+        'AB120',
+        'AB121',
+        'AB122',
+        'AB123',
+        'AB124',
+        'AB125',
+        'AB126',
+        'AB127',
+        'AB128',
+        'AB129',
+        'AB130',
+        'AB131',
+        'AB132',
+        'AB133',
+        'AB134',
+        'AB135',
+        'AB136',
+        'AB137',
+        'AB138',
+        'AB139',
+        'AB140',
+        'AB141',
+        'AB142',
+        'AB143',
+        'AB144',
+        'AB145',
+        'AB147',
+        'AB149',
+        'AB150',
+        'AB151',
+        'AB152',
+        'AB153',
+        'AB154',
+        'AB155',
+        'AB156',
+        'AB157',
+        'AB158',
+        'AB159',
+        'AB161',
+        'AB162',
+        'AB163',
+        'AB164',
+    ]
+    mouse_ids = mouse_ids + mouse_ids_mh
+    mouse_ids = ['MH035']
 
 
-    session_not_to_do = []
-    experimenter = 'JL'    
-    experimenter_full = 'Jules_Lebert'
+    session_not_to_do = ['MH007_20250128_110740', 'MH007_20250128_110814']
+
+    experimenter = 'AB'
+    experimenter_full = 'Axel_Bisi'
     # last_done_day = '20240506'
     last_done_day = None
     skip_existing_files = False # Overwrite if False
-    n_jobs = 15
+    n_jobs = 30
     sessions_to_convert = []
 
     for mouse_id in mouse_ids:
@@ -180,6 +262,7 @@ if __name__ == '__main__':
             continue
         analysis_folder = get_subject_analysis_folder(mouse_id, experimenter=experimenter_full)
         nwb_folder = get_nwb_folder(mouse_id, experimenter=experimenter_full)
+        nwb_folder = r"M:\analysis\Axel_Bisi\NWB_new"
 
         sessions_done = Path(nwb_folder).glob('*.nwb')
         sessions_done = [f.stem for f in sessions_done]
@@ -190,30 +273,35 @@ if __name__ == '__main__':
         for isession, iday in training_days:
 
             # Filter session ID to do.
-            if isession not in sessions_to_do:
-                continue
+            #if isession not in sessions_to_do:
+            #    continue
 
-            if skip_existing_files:
-                session_not_to_do = session_not_to_do + sessions_done
-            if isession in session_not_to_do:
-                print(f'Skipping {isession}')
-                continue
+           # if skip_existing_files:
+           #     session_not_to_do = session_not_to_do + sessions_done
+            #if isession in session_not_to_do:
+            #    print(f'Skipping {isession}')
+            #    continue
 
             # Filter by date.
             session_date = isession.split('_')[1]
             session_date = datetime.datetime.strptime(session_date, "%Y%m%d")
-            if last_done_day is not None:
-                if session_date <= datetime.datetime.strptime(last_done_day, "%Y%m%d"):
-                    continue
-                else:
-                    print('Converting', isession)
+
+            #if last_done_day is not None:
+            #    if session_date <= datetime.datetime.strptime(last_done_day, "%Y%m%d"):
+            #        continue
+            #    else:
+            #        print('Converting', isession)
 
             # Filter by session type.
             last_session_type = training_days[-1][1]
-            if experimenter == 'AB' and iday not in ['whisker_0']:
-                continue
-            elif experimenter == 'PB' and iday!=last_session_type:
-                continue
+
+            #if experimenter == 'AB' and iday not in ['whisker_0']:
+            #    continue
+            #if experimenter == 'MH' and iday not in ['whisker_0']: # not in ['whisker_0', 'whisker_+1', 'whisker_+2', 'whisker_+3', 'whisker_+4']:
+            #    continue
+            #elif experimenter == 'PB' and iday!=last_session_type:
+            #    continue
+
             print('Converting', isession)
 
             # Find yaml config file and behavior results for this session.
